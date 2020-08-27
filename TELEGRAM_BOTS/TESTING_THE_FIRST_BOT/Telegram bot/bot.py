dbcdc43f3f73667e71e61ebbff5bea80e7322073
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-08-28 02:18:57 +0300

    Add files via upload

--- TELEGRAM_BOTS/TESTING_THE_FIRST_BOT/Telegram bot/bot.py
+++ TELEGRAM_BOTS/TESTING_THE_FIRST_BOT/Telegram bot/bot.py
@@ -1,11 +1,12 @@
 import requests
+import json
 import random
 import pickle
 import telebot
 from telebot.types import Message
 from telebot import types
 
-TOKEN = ''
+TOKEN = '1379710116:AAG9BmigUQlgZKnQ3_JEWqKo--7HbbiF1O0'
 STICKER_ID='CAACAgIAAxkBAAN1X0aeJkafPIr1haxKJEvF1nRDPToAAsEAA2sLFBR5KCryovaX0xsE' # найдите стикос по души и отправляйте его :)
 
 bot=telebot.TeleBot(TOKEN)
@@ -20,15 +21,36 @@ def commands_and_handler(message:Message):
 @bot.edited_message_handler(content_types=["text"])
 @bot.message_handler(content_types=["text"]) #Бот ждет сообщение и если происходит событие - текст, он выполняет функцию
 def echo_digits(message:Message):
-    reply=str(random.randint(0,100))
 
+    markup = types.ReplyKeyboardMarkup()
+    itembtna = types.KeyboardButton('/start')
+    itembtnv = types.KeyboardButton('v')
+    #itembtnc = types.KeyboardButton('c')
+    #itembtnd = types.KeyboardButton('d')
+    #itembtne = types.KeyboardButton('e')
+    markup.row(itembtna, itembtnv)
+    #markup.row(itembtnc, itembtnd, itembtne)
+    #bot.send_message(message.chat.id, "Choose one letter:", reply_markup=markup)
+
+    with open ("saves.json" ) as f: #Открытие файла  
+        info=json.load(f)
+        print ("opening data...") #Лоигрование, что запись в файл идет
+    print (info)
+
+
+
+    reply=str(random.randint(0,100))
     if 'alexa' in message.text:
         bot.reply_to(message,str("Pishov Nahui"))
         return 
-    if message.from_user.id in USERS:
+    if str (message.from_user.id) in info:
         reply+=f" {message.from_user.first_name} thats your new number" 
     else:
-        USERS.add(message.from_user.id)
+        with open ("saves.json", "w") as f:
+            print ("saving data...")
+            data= str(info) + str(message.from_user.id)+","
+            json.dump (data, f)
+            print (data)
     bot.reply_to(message,reply)
 
 
@@ -45,5 +67,6 @@ def query_text(inline_query):
         bot.answer_inline_query(inline_query.id, [r, r2])
     except Exception as e:
         print(e)
+
 
 bot.polling(timeout=60)
