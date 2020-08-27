db5162f2e8bf30ab8fe24963895fef3ecb534b41
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-08-28 02:30:30 +0300

    Add files via upload

--- TELEGRAM_BOTS/TESTING_THE_FIRST_BOT/Telegram bot/bot.py
+++ TELEGRAM_BOTS/TESTING_THE_FIRST_BOT/Telegram bot/bot.py
@@ -25,12 +25,13 @@ def echo_digits(message:Message):
     markup = types.ReplyKeyboardMarkup()
     itembtna = types.KeyboardButton('/start')
     itembtnv = types.KeyboardButton('v')
+    itembtnz = types.KeyboardButton('z')
     #itembtnc = types.KeyboardButton('c')
     #itembtnd = types.KeyboardButton('d')
     #itembtne = types.KeyboardButton('e')
-    markup.row(itembtna, itembtnv)
+    markup.row(itembtna, itembtnv , itembtnz)
     #markup.row(itembtnc, itembtnd, itembtne)
-    #bot.send_message(message.chat.id, "Choose one letter:", reply_markup=markup)
+    bot.send_message(message.chat.id,"Choose what you want to do !", reply_markup=markup)
 
     with open ("saves.json" ) as f: #Открытие файла  
         info=json.load(f)
@@ -57,16 +58,17 @@ def echo_digits(message:Message):
 @bot.message_handler(content_types=["sticker"])
 def stickos (message:Message):
     bot.send_sticker(message.chat.id, STICKER_ID)
+    print (message)
 
-@bot.inline_handler(lambda query: query.query == 'text')
-def query_text(inline_query):
-    print (inline_query)
-    try:
-        r = types.InlineQueryResultArticle('1', 'Result', types.InputTextMessageContent('Result message.'))
-        r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('Result message2.'))
-        bot.answer_inline_query(inline_query.id, [r, r2])
-    except Exception as e:
-        print(e)
+# @bot.inline_handler(lambda query: query.query == 'text')
+# def query_text(inline_query):
+#     print (inline_query)
+#     try:
+#         r = types.InlineQueryResultArticle('1', 'Result', types.InputTextMessageContent('Result message.'))
+#         r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('Result message2.'))
+#         bot.answer_inline_query(inline_query.id, [r, r2])
+#     except Exception as e:
+#         print(e)
 
 
 bot.polling(timeout=60)
