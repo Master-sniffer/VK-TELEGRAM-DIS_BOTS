05a5eac3b6a71e5f72a098d2b27c57ada7ceef5e
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-10-03 15:11:26 +0300

    Update bot.py

--- TELEGRAM_BOTS/TESTING_THE_FIRST_BOT/SECOND_BOT/bot.py
+++ TELEGRAM_BOTS/TESTING_THE_FIRST_BOT/SECOND_BOT/bot.py
@@ -15,47 +15,51 @@ from datetime import date, datetime
 
 TOKEN = ''
 
-bot=telebot.TeleBot(TOKEN) # Тут типа 
+bot=telebot.TeleBot(TOKEN) 
 
 USERS=set()  # Юзаем множество вместо словаря 
 
 @bot.edited_message_handler(content_types=["text"])
 @bot.message_handler(content_types=["text"]) #Бот ждет сообщение и если происходит событие - текст, он выполняет функцию
 def echo_digits(message:Message):
-    if message.text == 'task_1':
-        bot.reply_to(message,str("some people say that the problem is just in contrast "))
-        bot.send_photo (message, "first_Task.png")
+    
+    #REPLY to message
+    #SEND TO message.chat.id
 
-    elif message.text=="H2020452178":
-        bot.send_message (message, "I know that you've done a lot\nbut it is just the \nb\ne\ng\ni\nn\nn\ni\nn\ng\n\nMy momma loves say'n that different languages sometimes help you to understand the whole meaning of the thing. Then AND only then u reverse the thing and get the answer\nGood luck, h@ckEr$\n\n")
-        bot.send_document(message, "task_2.gif")
+    if 'task_1' in message.text:
+        bot.reply_to(message,str("some people say that the problem is just in contrast "))  
+        bot.send_photo (message.chat.id, "first_Task.png")
+
+    elif 'H2020452178' in message.text:
+        bot.send_message (message.chat.id, "I know that you've done a lot\nbut it is just the \nb\ne\ng\ni\nn\nn\ni\nn\ng\n\nMy momma loves say'n that different languages sometimes help you to understand the whole meaning of the thing. Then AND only then u reverse the thing and get the answer\nGood luck, h@ckEr$\n\n")
+        bot.send_document(message.chat.id, "task_2.gif")
     
-    elif message.text == "спом":
-        bot.send_message (message, "Great job, brother\nRemember what u've written. Now i give an another challenge.\nLet me give u an advice\nHow high can u get when u... Chill...\n\n51.1788293\n-1.826183")
+    elif 'спом' in message.text:
+        bot.send_message (message.chat.id, "Great job, brother\nRemember what u've written. Now i give an another challenge.\nLet me give u an advice\nHow high can u get when u... Chill...\n\n51.1788293\n-1.826183")
     
-    elif message.text == "99":
-        bot.send_message(message, "AHAHAHAHA\n U've REALlaadlaLY DADANDANDONE IT\nCONGRATS !\n\n\n\nДыаыит фб цфбгч, эёгые ябгч ат егтфч !")
+    elif '99' in message.text :
+        bot.send_message(message.chat.id, "AHAHAHAHA\n U've REALlaadlaLY DADANDANDONE IT\nCONGRATS !\n\n\n\nДыаыит фб цфбгч, эёгые ябгч ат егтфч !")
     
-    elif message.text == "ROT19":
-        bot.send_message(message, "I know that u've been doing not alone and we appreciate that, but we need persons... Leaders, not the followers. This is why, i give u an another challenge which gonna make u cry\n")
-        bot.send_message(message, "Just, please, remember that some words in one language should be written in another one))0)0)")
-        bot.send_document(message, "task.rar")
+    elif 'ROT19' in message.text:
+        bot.send_message(message.chat.id, "I know that u've been doing not alone and we appreciate that, but we need persons... Leaders, not the followers. This is why, i give u an another challenge which gonna make u cry\n")
+        bot.send_message(message.chat.id, "Just, please, remember that some words in one language should be written in another one))0)0)")
+        bot.send_document(message.chat.id, "task.rar")
 
 
-    elif message.text=="Dont be afraid, but ur answer is...":
-        bot.send_message(message, "quite tricky , ain't it ?")
-        bot.send_message(message, "tam-tam-tam\nta ka di mi\nta ka di mi\nrecognize it, man ?)\nwell, sometimes u say it faster, sometimes slower, but why...\ndamn, i dont know\n\n\n")
-        bot.send_audio(message, "Max-Richter-November.mp3")
+    elif 'Dont be afraid, but ur answer is...' in message.text:
+        bot.send_message(message.chat.id, "quite tricky , ain't it ?")
+        bot.send_message(message.chat.id, "tam-tam-tam\nta ka di mi\nta ka di mi\nrecognize it, man ?)\nwell, sometimes u say it faster, sometimes slower, but why...\ndamn, i dont know\n\n\n")
+        bot.send_audio(message.chat.id, "Max-Richter-November.mp3")
     
-    elif message.text =="81":
-        bot.send_message(message, "just try to remember what u say and the answer will be there\n")
-        bot.send_document(message, "deepAF.rar")
+    elif '81' in message.text :
+        bot.send_message(message.chat.id, "just try to remember what u say and the answer will be there\n")
+        bot.send_document(message.chat.id, "deepAF.rar")
     
-    elif message.text=="онлию":
-        bot.send_message (message, "Ты хорошо постарался и теперь тебя ждет награда, друг мой\nДержи\nhttps://vk.com/finuniversity\n\nбыло весело, удачи тебе")
+    elif 'онлию' in message.text :
+        bot.send_message (message.chat.id, "Ты хорошо постарался и теперь тебя ждет награда, друг мой\nДержи\nhttps://vk.com/finuniversity\n\nбыло весело, удачи тебе")
     
     else:
-        bot.send_message(message,"Want to start the game ? Enter\ttask_1\n\nneed help ?\nwrite me\nhttps://vk.com/masster_sniffer")
+        bot.send_message(message.chat.id,"Want to start the game ? Enter\ttask_1\n\nneed help ?\nwrite me\nhttps://vk.com/masster_sniffer")
 
     
 
